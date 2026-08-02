b419d3cff63fd87c7049256585fc1d1ddb6329a5
asha514 <ashababu1811@gmail.com>
2026-08-02 15:55:59 +0530

    debug email

--- config/email.js
+++ config/email.js
@@ -1,24 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.log("Mail Error:", err);
-  } else {
-    console.log("SMTP Connected Successfully");
-  }
 });
 
 module.exports = transporter;
