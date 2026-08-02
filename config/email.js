f47d3f587d848c12a8c766f354d1c38bc2d3524f
asha514 <ashababu1811@gmail.com>
2026-08-02 16:07:10 +0530

    debug email

--- config/email.js
+++ config/email.js
@@ -1,11 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.error("SMTP Verify Error:", err);
+  } else {
+    console.log("SMTP Connected Successfully");
+  }
 });
 
 module.exports = transporter;
