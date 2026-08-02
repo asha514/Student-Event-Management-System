8cdac66ae3a24bde4712be4d63c5f7b9c50925f1
asha514 <ashababu1811@gmail.com>
2026-08-02 17:31:55 +0530

    debug email

--- config/email.js
+++ config/email.js
@@ -1,11 +1,10 @@
-const dns = require("dns");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4, // Force IPv4
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -14,3 +13,5 @@ const transporter = nodemailer.createTransport({
     rejectUnauthorized: false,
   },
 });
+
+module.exports = transporter;
